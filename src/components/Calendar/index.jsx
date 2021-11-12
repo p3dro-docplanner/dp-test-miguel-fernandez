1544ca822cce69d07777d3709cb0f3b2f4dbf0b4
Miguel Fernandez <miguelfervi@gmail.com>
2021-11-12 13:48:02 +0100

    Add momentjs and function to group by day of week

--- src/components/Calendar/index.jsx
+++ src/components/Calendar/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import appointmentService from "../../services/appointmentService";
+import moment from "moment";
 
 export const Calendar = () => {
   const [appointments, setAppointments] = useState([]);
@@ -11,15 +12,37 @@ export const Calendar = () => {
     });
   }, []);
 
+  const groupsByDay =
+    appointments &&
+    appointments.reduce((acc, date) => {
+      const weekDay = `${moment(date.Start).week()}-${moment(
+        date.Start
+      ).day()}`;
+
+      if (!acc[weekDay]) {
+        acc[weekDay] = [];
+      }
+
+      acc[weekDay].push(date);
+      return acc;
+    }, {});
+
+  const groupPerDay = () => {
+    /* const startDate = moment(appointment.Start);
+      const weekDay = startDate.format("ddd");
+      const numberDay = startDate.format("DD");
+      const monthName = startDate.format("MMM");
+      const hour = startDate.format("HH");
+      const minutes = startDate.format("mm");*/
+
+    console.log(groupsByDay);
+
+    //return <div>{new Date(appointment.Start).getDate()}</div>;
+  };
+
   return (
-    <div>
-      {appointments.length > 0 && (
-        <div>
-          {appointments.map((appointment) => (
-            <div>{new Date(appointment.Start).getDate()}</div>
-          ))}
-        </div>
-      )}
+    <div className="current-appointment">
+      {appointments.length > 0 && <div>{groupPerDay()}</div>}
     </div>
   );
 };
