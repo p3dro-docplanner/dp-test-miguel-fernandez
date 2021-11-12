68b038a206e67af32769e148e2b975fad3bbc88a
Miguel Fernandez <miguelfervi@gmail.com>
2021-11-12 23:08:02 +0100

    Remove console log

--- src/components/Reschedule/index.jsx
+++ src/components/Reschedule/index.jsx
@@ -3,7 +3,6 @@ import { useSelector } from "react-redux";
 
 export const Reschedule = () => {
   const appointment = useSelector((state) => state.appointment);
-  console.log(appointment);
 
   return appointment.changed ? <div>Here</div> : <div>Fake</div>;
 };
