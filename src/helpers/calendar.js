1cda9ed539d08017f80703bd3510d18e3a78cf63
Miguel Fernandez <miguelfervi@gmail.com>
2021-11-13 09:25:17 +0100

    refactor function

--- src/helpers/calendar.js
+++ src/helpers/calendar.js
@@ -49,15 +49,5 @@ export const enumerateDaysBetweenDates = (date) => {
 };
 
 export const isWeekRange = (day) => {
-  /*   console.log(moment().format("YYYY-MM-DD"));
-  console.log(moment().add(5, "days").format("YYYY-MM-DD"));
-  // true
-
-  return moment("2021-11-15").isBetween(
-    moment().format("YYYY-MM-DD"),
-    "2021-11-17"
-  );*/
-  console.log(day);
-  console.log(moment().isSameOrBefore("2021-11-17"));
-  return parseDate(day.Start).isSameOrBefore("2021-11-17");
+  return parseDate(day.Start).isSameOrBefore(moment().add(6, "days"), "year");
 };
